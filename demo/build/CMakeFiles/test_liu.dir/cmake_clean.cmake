e45b7f4999f91f554d1960a3789d06bc7381e0a8
Qianyong Liu <qianyong.liu@taurus.ai>
2020-10-05 09:12:47 +0800

    add git ignore

---
+++ demo/build/CMakeFiles/test_liu.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_liu.dir/main.cpp.o"
+  "test_liu"
+  "test_liu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_liu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
